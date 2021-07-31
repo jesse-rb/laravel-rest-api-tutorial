37456c827577fcdb00c8ada7816e1e2b9a8a51ef
jesse-rb <jesse.reynekebarnard@gmail.com>
2021-07-31 20:35:29 +1200

    Add controller and routes for Seller model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,6 +15,7 @@ class Product extends Model
         'name',
         'slug',
         'description',
-        'price'
+        'price',
+        'seller_id'
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SellerController;
 use App\Http\Controllers\AuthController;
 
 /*
@@ -18,18 +19,39 @@ use App\Http\Controllers\AuthController;
 
 // Route::resource('products', ProductController::class); // All standard crud routes
 
+//
 // Public routes
+//
+
+// Product routes
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+// Seller routes
+Route::get('/sellers', [SellerController::class, 'index']);
+Route::get('/sellers/{id}', [SellerController::class, 'show']);
+Route::get('/sellers/search/{name}', [SellerController::class, 'search']);
+
+// Auth routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+//
 // Protected routes (through sanctum )
+//
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Product routes
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    // Seller routes
+    Route::post('/sellers', [SellerController::class, 'store']);
+    Route::put('/sellers/{id}', [SellerController::class, 'update']);
+    Route::delete('/sellers/{id}', [SellerController::class, 'destroy']);
+
+    // Auth routes
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
